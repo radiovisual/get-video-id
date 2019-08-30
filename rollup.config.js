972202938e1b839e534fc87dfdb1e219ecc8a760
Otavio Araujo <otaviotech@gmail.com>
2019-08-30 00:40:54 -0300

    Adding browser and esModules support using rollup.

---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import {terser} from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const minified = file => file.replace(/.js/, '.min.js');
+
+export default {
+	input: 'index.js',
+
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs'
+		},
+		{
+			file: minified(pkg.main),
+			format: 'cjs'
+		},
+		{
+			file: pkg.module,
+			format: 'es'
+		},
+		{
+			file: minified(pkg.module),
+			format: 'es'
+		},
+		{
+			file: pkg.browser,
+			format: 'iife',
+			name: 'getVideoId'
+		},
+		{
+			file: minified(pkg.browser),
+			format: 'iife',
+			name: 'getVideoId',
+		}
+	],
+
+	plugins: [
+		resolve({
+			mainFields: ['module', 'main', 'browser'],
+		}),
+		commonjs(),
+		babel({
+			exclude: 'node_modules/**',
+			babelrc: false,
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						modules: false
+					}
+				]
+			]
+		}),
+		terser({
+			include: [/^.+\.min\.js$/]
+		})
+	],
+};
